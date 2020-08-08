2e5cc95bcad612c1a4d1a7bc81409bee21a27194
rajapradeep <7948650+praddevops@users.noreply.github.com>
2020-08-08 17:04:13 -0400

    Update petstore3-service.tf

--- petstore3-service.tf
+++ petstore3-service.tf
@@ -32,7 +32,7 @@ resource "aws_ecs_service" "petstore3" {
     cluster = "${aws_ecs_cluster.main.id}"
     task_definition = "${aws_ecs_task_definition.petstore3.arn}"
     iam_role = "${aws_iam_role.ecs_service_role.arn}"
-    desired_count = 1
+    desired_count = 2
     depends_on = [aws_iam_role_policy.ecs_service_role_policy]
 
     load_balancer {
